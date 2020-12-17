df367aaf80feff5eabdb48b81743b2735b7c4439
juniaraldi <aldinugraha708@gmail.com>
2020-12-17 19:39:31 +0700

    label:"📚 documentation"

--- asset/js/main.js
+++ asset/js/main.js
@@ -41,3 +41,4 @@ $(document).ready(function(){
 var dt = new Date();
 document.getElementById("tanggalwaktu").innerHTML = (("0"+dt.getDate()).slice(-2)) +"."+ (("0"+(dt.getMonth()+1)).slice(-2)) +"."+ (dt.getFullYear()) +" "+ (("0"+dt.getHours()).slice(-2)) +":"+ (("0"+dt.getMinutes()).slice(-2));
 
+
